4e0bfe21a1dfb953a3a03ba03ebe83f73bd7a557
PavanPochi06 <pavanpochi06@gmail.com>
2026-01-23 18:11:50 +0530

    Lift cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { HomePage } from "./pages/HomePage";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
+import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/cart-items")
+      .then((response) => {
+        setCart(response.data);
+      })
+      .catch(() => {});
+  }, []);
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,8 +1,7 @@
 import "./header.css";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
-
-export function Header({cart}){
+export function Header({ cart = [] }) {
   let totalQuantity = 0;
 
   cart.forEach((cartItem) => {
@@ -10,34 +9,33 @@ export function Header({cart}){
   });
 
   return (
-      <div className="header">
-        <div className="left-section">
-          <Link to="/" className="header-link">
-            <div className="logo">ECOMMERCE WEBSITE</div>
-            <div className="mobile-logo">ECOMMERCE WEBSITE</div>
-          </Link>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <Link className="orders-link header-link" to="/orders">
-
-            <span className="orders-text">Orders</span>
-          </Link>
-
-          <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">{totalQuantity}</div>
-            <div className="cart-text">Cart</div>
-          </Link>
-        </div>
+    <div className="header">
+      <div className="left-section">
+        <Link to="/" className="header-link">
+          <div className="logo">ECOMMERCE WEBSITE</div>
+          <div className="mobile-logo">ECOMMERCE WEBSITE</div>
+        </Link>
       </div>
+
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
+
+        <button className="search-button">
+          <img className="search-icon" src="images/icons/search-icon.png" />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <Link className="orders-link header-link" to="/orders">
+          <span className="orders-text">Orders</span>
+        </Link>
+
+        <Link className="cart-link header-link" to="/checkout">
+          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <div className="cart-quantity">{totalQuantity}</div>
+          <div className="cart-text">Cart</div>
+        </Link>
+      </div>
+    </div>
   );
 }
